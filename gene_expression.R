e814f8b500f6c9749322a78ed7e394f6982c66e4
Camila Sinimbú <camila.forte@itec.ufpa.br>
2024-08-13 16:55:20 -0300

    Add files via upload

--- gene_expression.R
+++ gene_expression.R
@@ -3,21 +3,36 @@ library(plyr)
 library(dplyr)
 library(purrr)
 library(stringr)
+library("TCGAutils")
 
 
-reader <- function(f){
-  df <- read.table(f, sep='\t', skip=6, header=FALSE)
-  colnames(df)[4] <- str_replace(basename(f), ".rna_seq.augmented_star_gene_counts.tsv", "")
+reader <- function(file, dir){
+  df <- read.table(file, sep='\t', skip=6, header=FALSE)
+  colnames(df)[4] <- dir
   df
 }
+
+dire <-list.dirs(path = "Gene_Expression_Quantification/", 
+                full.names = TRUE, recursive = TRUE)
+dir <- sort(basename(dire))
+list_dir <- dir[-length(dir)]
+
 
 files <- list.files("Gene_Expression_Quantification/", 
                     recursive=TRUE, full.names=TRUE)
 
-myfilelist <- lapply(files, reader)
+myfilelist <- lapply(files, reader, dir=list_dir)
 
 
 f_df <- bind_cols(myfilelist[[1]][1:2],
           imap(myfilelist, \(df, i) select(df, 4)) |> bind_cols())
 
 write.table(f_df, "TCGA_STAD_counts.txt", sep = '\t')
+
+
+UUIDtoBarcode("ae55b2d3-62a1-419e-9f9a-5ddfac356db4", from_type = "case_id")
+
+
+library("TCGAutils")
+
+UUIDtoBarcode("", from_type="file_id")
